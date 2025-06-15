dfea465b511e8183b38d17bfcf23576f692c3295
xuexcy <xuechengyun@gmail.com>
2025-06-15 23:02:39 +0800

    add get type_name

--- cpp_utils/util.h
+++ cpp_utils/util.h
@@ -14,6 +14,28 @@
 
 namespace cpp_utils {
 
+template <typename T>
+constexpr auto type_name() {
+  std::string_view name = CURRENT_FUNCTION_NAME;
+  std::string_view prefix, suffix;
+#ifdef __clang__
+//  name = __PRETTY_FUNCTION__;
+  prefix = "auto cpp_utils::type_name() [T = ";
+  suffix = "]";
+#elif defined(__GNUC__)
+ // name = __PRETTY_FUNCTION__;
+  prefix = "constexpr auto cpp_utils::type_name() [with T = ";
+  suffix = "]";
+#elif defined(_MSC_VER)
+  //name = __FUNCSIG__;
+  prefix = "auto __cdecl cpp_utils::type_name<";
+  suffix = ">(void)";
+#endif
+  name.remove_prefix(prefix.size());
+  name.remove_suffix(suffix.size());
+  return name;
+}
+
 template <class T>
 inline const T* object_end_address(const T* obj_ptr) {
     // void* end = (void*)(obj_ptr + 1);
